f547b56c17ce0a7c80f3d9343a34cdd65f4d4a23
OmerKarp <omerkarp1306@gmail.com>
2025-02-08 01:36:02 +0200

    deleted unnecessary svg and png, and fixed to Hero section positions

--- portfolio_website/src/constants/index.js
+++ portfolio_website/src/constants/index.js
@@ -243,31 +243,31 @@ export const calculateSizes = (isSmall, isMobile, isTablet) => {
     cubePosition: isSmall
       ? [4, -5, 0]
       : isMobile
-      ? [5, -5, 0]
+      ? [7, -5, 0]
       : isTablet
-      ? [5, -5, 0]
-      : [9, -5.5, 0],
+      ? [9, -5, 0]
+      : [13, -5.5, 0],
     reactLogoPosition: isSmall
       ? [3, 4, 0]
       : isMobile
-      ? [5, 4, 0]
+      ? [5, 2, 0]
       : isTablet
-      ? [5, 4, 0]
-      : [12, 3, 0],
+      ? [11, 4, 0]
+      : [14.5, 3, 0],
     ringPosition: isSmall
       ? [-5, 7, 0]
       : isMobile
-      ? [-10, 10, 0]
+      ? [-12, 6, 0]
       : isTablet
-      ? [-12, 10, 0]
-      : [-24, 10, 0],
+      ? [-22, 10, 0]
+      : [-30, 10, 0],
     targetPosition: isSmall
-      ? [-5, -10, -10]
+      ? [-5, -10, -12]
       : isMobile
-      ? [-9, -10, -10]
+      ? [-9, -10, -12]
       : isTablet
-      ? [-11, -7, -10]
-      : [-13, -13, -10],
+      ? [-11, -7, -12]
+      : [-17, -13, -12],
   };
 };
 
