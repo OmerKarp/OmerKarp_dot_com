834821d3ef5c4f5d9df3137785ffb8e9639f2f52
OmerKarp <omerkarp1306@gmail.com>
2025-02-08 13:34:52 +0200

    cleaned up the code

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import WorkExperience from "./sections/Experience.jsx";
 
 const App = () => {
   return (
-    <main className="max-w-7xl mx-auto relative">
+    <main className="max-w-7xl lg:max-w-7xl mx-auto relative">
       <Navbar />
       <Hero />
       <About />
